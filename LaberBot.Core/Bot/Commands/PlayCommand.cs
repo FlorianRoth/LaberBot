6ce6afeed034f3cc4829ff57a00a7103cacb1790
Florian Roth <rothflorian@gmx.de>
2017-03-08 20:19:49 +0100

    Support playing to a certain user with the !play command

--- LaberBot.Core/Bot/Commands/PlayCommand.cs
+++ LaberBot.Core/Bot/Commands/PlayCommand.cs
@@ -2,13 +2,19 @@
 {
     using System;
     using System.ComponentModel.Composition;
+    using System.Linq;
     using System.Threading.Tasks;
 
+    using Discord;
     using Discord.Commands;
 
     [Export(typeof(IBotCommand))]
     public class PlayCommand : BotCommandBase
     {
+        private const string ARG_FILE = "file";
+
+        private const string ARG_USER = "user";
+
         private readonly IAudioPlayer _player;
 
         private readonly ISoundRepository _soundRepository;
@@ -21,27 +27,45 @@
         {
             _player = player;
             _soundRepository = soundRepository;
-            Parameter = new[] { Tuple.Create("file", ParameterType.Required) };
+            Parameter = new[]
+            {
+                Tuple.Create(ARG_FILE, ParameterType.Required),
+                Tuple.Create(ARG_USER, ParameterType.Optional)
+            };
         }
 
         public override async Task ExecuteAsync(CommandEventArgs args)
         {
-            var voiceChannel = args.User.VoiceChannel;
-            if (null == voiceChannel)
-            {
-                return;
-            }
-
-            var sound = args.GetArg(0);
+            var sound = args.GetArg(ARG_FILE);
             var file = _soundRepository.GetSoundFile(sound);
-            
             if (null == file)
             {
                 await SendMessageToChannelAsync(args, $"Sound '{sound}' does not exist");
                 return;
             }
 
+            var voiceChannel = GetVoiceChannel(args);
+            if (null == voiceChannel)
+            {
+                await SendMessageToChannelAsync(args, "No voice channel could be found");
+                return;
+            }
+            
             await _player.PlayAsync(voiceChannel, file);
+        }
+
+        private Channel GetVoiceChannel(CommandEventArgs args)
+        {
+            var userName = args.GetArg(ARG_USER);
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                return args.User.VoiceChannel;
+            }
+
+            var user = args.Server.FindUsers(userName, true).FirstOrDefault();
+
+            return user?.VoiceChannel;
         }
     }
 }
